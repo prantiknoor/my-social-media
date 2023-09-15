306e30aacc42be9ab13c9ed5ac547175cd5a515b
Prantik <prantiknoor@gmail.com>
2023-09-15 22:44:09 +0600

    Increase maxLength of password property of User

--- src/models/User.js
+++ src/models/User.js
@@ -22,7 +22,7 @@ const userSchema = new Schema(
         password: {
             type: String,
             minLength: 6,
-            maxLength: 30,
+            maxLength: 50,
             required: true,
         },
         role: {
