15bdb75ac77e49b501930f89447eaa2662b4ceab
Prantik <prantiknoor@gmail.com>
2023-09-16 18:16:16 +0600

    added counter related functionalities

---
+++ src/lib/counter/index.js
@@ -0,0 +1,38 @@
+const { Counter } = require("../../models")
+const { notFound } = require("../../utils/httpErrors")
+
+const createCounter = async ({ parent, type }) => {
+    const counterExist = await Counter.findOne({ parent, type })
+
+    if (counterExist) throw new Error('Counter already exist ')
+
+    const counter = await Counter.create({ parent, type })
+
+    return counter
+}
+
+const findCounter = async ({ parent, type }) => {
+    const counter = await Counter.findOne({ parent, type })
+
+    if (!counter) throw notFound()
+
+    return counter
+}
+
+const incrementCounter = async ({ parent, type, incrementBy = 1 }) => {
+    const counter = await Counter.findOne({ parent, type })
+
+    if (!counter) throw notFound()
+
+    const count = counter.count + incrementBy
+
+    await counter.updateOne({ count })
+
+    return counter
+}
+
+module.exports = {
+    createCounter,
+    findCounter,
+    incrementCounter
+}
