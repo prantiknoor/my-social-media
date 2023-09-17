eadedafe872f8229ba015e7dc2159c07cd2337d7
Prantik <prantiknoor@gmail.com>
2023-09-17 13:20:06 +0600

    Add follower related functionalities

---
+++ src/lib/follow/index.js
@@ -0,0 +1,37 @@
+const { Follower, User } = require("../../models")
+const { notFound } = require("../../utils/httpErrors")
+const { incrementCounter } = require("../counter")
+
+const follow = async ({ followee, follower }) => {
+    let code = 200
+    let followerObj = await Follower.findOne({ followee, follower })
+
+    if (!followerObj) {
+        const followeeExist = await User.findById(followee)
+
+        if (!followeeExist) throw notFound('Followee not found')
+
+        followerObj = await Follower.create({ followee, follower })
+        code = 201
+
+        await incrementCounter({ parent: followee, type: 'followers', incrementBy: 1 })
+        await incrementCounter({ parent: follower, type: 'followees', incrementBy: 1 })
+    }
+
+    return { data: followerObj._doc, code }
+}
+
+const unfollow = async ({ followee, follower }) => {
+    let followerObj = await Follower.findOne({ followee, follower })
+
+    if (!followerObj) return { code: 200 }
+
+    await followerObj.deleteOne()
+
+    await incrementCounter({ parent: followee, type: 'followers', incrementBy: -1 })
+    await incrementCounter({ parent: follower, type: 'followees', incrementBy: -1 })
+
+    return { code: 204 }
+}
+
+module.exports = { follow, unfollow }
