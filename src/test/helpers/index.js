bc2af1b3e5563bd1801868c35064e94ac19e2696
Prantik <prantiknoor@gmail.com>
2023-09-18 19:26:02 +0600

    Add createRandomPost function

--- src/test/helpers/index.js
+++ src/test/helpers/index.js
@@ -1,5 +1,6 @@
 const { faker } = require('@faker-js/faker')
 const { createUser } = require('../../lib/user')
+const { createPost } = require('../../lib/post')
 
 const getFakeUserCreationData = () => ({
     name: faker.person.fullName(),
@@ -16,4 +17,21 @@ const createRandomUser = async () => {
     return { data, user }
 }
 
-module.exports = { getFakeUserCreationData, createRandomUser }
+const createRandomPost = async ({ commenting, audience, creator = '6506d176b40d591e4fddb5e4' }) => {
+    const data = {
+        body: faker.lorem.paragraph(),
+        creator,
+        audience,
+        commenting,
+    }
+
+    const post = await createPost({ ...data })
+
+    return { data, post }
+}
+
+module.exports = {
+    getFakeUserCreationData,
+    createRandomUser,
+    createRandomPost
+}
