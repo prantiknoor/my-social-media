657bf4267feeb6be5d36e8bd47d6cb29786b1e45
Prantik <prantiknoor@gmail.com>
2023-09-18 19:24:02 +0600

    Update connectDB only log for error

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const connectDB = async (testDB = false) => {
     const dbName = testDB ? 'test' : 'main'
 
-    await mongoose.connect('mongodb://127.0.0.1:27017/' + dbName);
-
-    console.log('Database connected successfully');
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/' + dbName);
+    } catch (error) {
+        console.log('Database connected failed');
+    }
 }
 
 module.exports = { connectDB }
