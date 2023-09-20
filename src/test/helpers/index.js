4faff9eebbf08c6952163b175506b910ced08d9f
Prantik <prantiknoor@gmail.com>
2023-09-20 18:25:52 +0600

    Add createRandomComment function

--- src/test/helpers/index.js
+++ src/test/helpers/index.js
@@ -1,6 +1,8 @@
 const { faker } = require('@faker-js/faker')
 const { createUser } = require('../../lib/user')
 const { createPost } = require('../../lib/post')
+const { createComment } = require('../../lib/comment')
+const { Comment } = require('../../models')
 
 const getFakeUserCreationData = () => ({
     name: faker.person.fullName(),
@@ -30,8 +32,24 @@ const createRandomPost = async ({ commenting, audience, creator = '6506d176b40d5
     return { data, post }
 }
 
+const createRandomComment = async (data) => {
+    const post = await createRandomPost({})
+
+    data = {
+        body: data?.body ?? faker.lorem.sentence(),
+        post: data?.post ?? post.id,
+        commentor: data?.commentor ?? '6506d176b40d591e4fddb5e4',
+        status: data?.status ?? 'public'
+    }
+
+    const comment = await Comment.create(data)
+
+    return { data, comment }
+}
+
 module.exports = {
     getFakeUserCreationData,
     createRandomUser,
-    createRandomPost
+    createRandomPost,
+    createRandomComment
 }
