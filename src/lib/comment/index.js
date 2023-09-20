210bdd1de201167da6a2aaf1cf0be3f42c8c5b46
Prantik <prantiknoor@gmail.com>
2023-09-20 18:27:09 +0600

    Add comment related functions

---
+++ src/lib/comment/index.js
@@ -0,0 +1,86 @@
+const defaults = require("../../config/defaults")
+const { Comment, Post } = require("../../models")
+const { notFound } = require("../../utils/httpErrors")
+
+const createComment = async ({ body, post, commentor, status }) => {
+    const postExist = await Post.findById(post)
+
+    if (!postExist) throw notFound('Post not found')
+
+    const comment = await Comment.create({ body, post, commentor, status })
+
+    return comment
+}
+
+const deleteComment = async (id) => {
+    const comment = await Comment.findById(id)
+
+    if (!comment) throw notFound()
+
+    await comment.deleteOne()
+}
+
+const findSingleComment = async (id) => {
+    const comment = await Comment.findById(id)
+
+    if (!comment) throw notFound('Comment not found')
+
+    return comment._doc
+}
+
+const findAllComment = async ({
+    page = defaults.page,
+    limit = defaults.limit,
+    sortType = defaults.sortType,
+    sortBy = defaults.sortBy,
+}) => {
+    const sort = (sortType === 'desc' ? '-' : '') + sortBy
+
+    const comments = await Comment.find()
+        .sort(sort)
+        .skip((page - 1) * limit)
+        .limit(limit)
+
+    return comments
+}
+
+const updateOrCreateComment = async (id, { body, post, commentor, status }) => {
+    let comment = await Comment.findById(id)
+
+    const payload = { body, post, commentor, status }
+
+    if (!comment) {
+        comment = await createComment({ ...payload })
+        return { comment, code: 201 }
+    }
+
+    await comment.overwrite(payload).save()
+
+    return { comment, code: 200 }
+}
+
+const updateComment = async (id, { body, post, commentor, status }) => {
+    let comment = await Comment.findById(id)
+
+    if (!comment) throw notFound()
+
+    const payload = {
+        body: body ?? comment.body,
+        post: post ?? comment.post,
+        commentor: commentor ?? comment.commentor,
+        status: status ?? comment.status,
+    }
+
+    await comment.overwrite(payload).save()
+
+    return comment
+}
+
+module.exports = {
+    createComment,
+    deleteComment,
+    findSingleComment,
+    findAllComment,
+    updateComment,
+    updateOrCreateComment
+}
