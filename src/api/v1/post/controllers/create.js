8e12e06da27b4542e51f9654ba1ac529b4d7af7d
Prantik <prantiknoor@gmail.com>
2023-09-22 21:21:44 +0600

    Add counter creation for likes and comments

--- src/api/v1/post/controllers/create.js
+++ src/api/v1/post/controllers/create.js
@@ -1,8 +1,21 @@
 const postService = require('../../../../lib/post')
+const counterService = require('../../../../lib/counter')
 
 const create = async (req, res, next) => {
     try {
-        const post = await postService.createPost(req.body)
+        const data = { ...req.body, creator: req.user.id }
+
+        const post = await postService.createPost(data)
+
+        await counterService.createCounter({
+            parent: post.id,
+            type: 'likes'
+        })
+
+        await counterService.createCounter({
+            parent: post.id,
+            type: 'comments'
+        })
 
         const response = {
             code: 201,
