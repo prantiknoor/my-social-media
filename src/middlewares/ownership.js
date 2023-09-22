6765f26e448632e5f4c2caabf69234089652dd53
Prantik <prantiknoor@gmail.com>
2023-09-22 22:41:52 +0600

    Add check for Comment

--- src/middlewares/ownership.js
+++ src/middlewares/ownership.js
@@ -1,21 +1,37 @@
 const postService = require("../lib/post")
+const commentService = require("../lib/comment")
 const { authorizationError } = require("../utils/httpErrors")
 
 const ownership = model => async (req, _res, next) => {
     if (req.user.role === 'admin') return next()
 
-    switch (model) {
-        case 'Article':
-            const isOwner = await postService.checkOwnership({
-                postId: req.params.id,
-                userId: req.user.id
-            })
+    let isOwner
 
-            if (isOwner) return next()
+    try {
+        switch (model) {
+            case 'Post':
+                isOwner = await postService.checkOwnership({
+                    postId: req.params.id,
+                    userId: req.user.id
+                })
 
-            return next(authorizationError())
-        default:
-            return next()
+                if (isOwner) return next()
+
+                return next(authorizationError())
+            case 'Comment':
+                isOwner = await commentService.checkOwnership({
+                    commentId: req.params.id,
+                    userId: req.user.id
+                })
+
+                if (isOwner) return next()
+
+                return next(authorizationError())
+            default:
+                return next()
+        }
+    } catch (error) {
+        next(error)
     }
 }
 
